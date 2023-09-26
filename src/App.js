9a423e3796bfdf39f298150fb2f211a98c1966c5
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2023-09-26 15:57:06 +0900

    タスクの追加機能の実装

--- src/App.js
+++ src/App.js
@@ -17,6 +17,23 @@ const todoItems = [
 export default class App extends Component {
   state = {
     todoList: todoItems,
+    activeItem: {
+      title: "",
+      completed: false,
+    }
+  }
+  handleChange = (e) => {
+    let {name, value} = e.target;
+
+    if(e.target.type === checkbox){
+      value = e.target.checked;
+    }
+
+    const activeItem = {...this.state.activeItem, [name]: value};
+    this.setState({activeItem});
+  }
+  handleSubmit = (item) => {
+    alert("Save :: " + JSON.stringify(item));
   }
   render() {
     return (
@@ -24,7 +41,11 @@ export default class App extends Component {
         <h1 className="text-uppercase text-center my-2">Todo Application</h1>
         <div className="row">
           <div className="col-8 col-md-6 mx-auto mt-2">
-            <TodoInput/>
+            <TodoInput
+            activeItem={this.state.activeItem}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            />
             <TodoList items={this.state.todoList}/>
           </div>
         </div>
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,18 +2,34 @@ import React, {Component} from "react";
 
 export default class TodoInput extends Component {
     render() {
+        const { activeItem, handleChange, handleSubmit } = this.props;
         return(
             <div className="card card-body">
                 <form action="">
                     <div className="form-group">
                     <label>Title</label>
-                    <input type="text" className="form-control" name="title" placeholder="todoに追加"/>
+                    <input
+                    type="text"
+                    className="form-control" 
+                    name="title" 
+                    placeholder="todoに追加"
+                    value={activeItem.title}
+                    onChange={handleChange}/>
                     <div className="form-group form-check">
-                        <input type="checkbox" name="completed" className="form-check-input"/>
+                        <input
+                        type="checkbox"
+                        name="completed"
+                        className="form-check-input"
+                        checked={activeItem.completed}
+                        onChange={handleChange}
+                        />
                     </div>
                     <label className="form-check-label">完了</label>
                     </div>
-                    <button type="submit" className="btn btn-primary mt-2">追加</button>
+                    <button
+                    type="submit"
+                    className="btn btn-primary mt-2"
+                    onClick={() => handleSubmit(activeItem)}>追加</button>
                 </form>
             </div>
         );
